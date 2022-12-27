60adb4326751aec9d6fdd6e756dbec3b0eeb555f
Jerry Fitzner <94945473+jerryfitzner@users.noreply.github.com>
2022-12-27 17:57:08 -0500

    Added React Components

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learning React - Yes!!
+          Learning React - Yes!
         </a>
       </header>
     </div>
