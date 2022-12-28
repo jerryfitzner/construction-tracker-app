33f1d6430b13d8e0f0822fd1a43c076edfee971c
Jerry Fitzner <jfitzner1@gmail.com>
2022-12-28 14:18:16 -0500

    Updated Routes

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,16 +1,35 @@
 // import logo from './logo.svg';
 import '../App.css';
 import { Route, Routes } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import SignIn from './SignIn';
+import Houses from './Houses';
 
 function App() {
+  const [cont, setCont] = useState(null);
+
+  useEffect(() => {
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((cont) = setCont(cont))
+      }
+    });
+  }, []);
 
 
   return (
-    <Routes>
-      <Route path='/' element={ <SignIn />} />
-
-    </Routes>
+    <div>
+      {cont ? (
+        <Routes>
+          <Route path='/' element={ <Houses /> } />
+        </Routes>
+      ):(
+        <Routes>
+          <Route path='/signin' element={ <SignIn /> } />
+        </Routes>
+      )}
+    </div>
+    
   );
 }
 
