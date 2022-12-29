7d7d580d30f164c8af6b52ec2a92272780219858
Jerry Fitzner <94945473+jerryfitzner@users.noreply.github.com>
2022-12-29 16:17:01 -0500

    Add GET/me route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+
+  get "/me", to: "contractors#show"
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
